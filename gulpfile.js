52e50d8dbf2770bcd1a8465316c7dfd79b9cbd1a
Alan <ajohnson88@gmail.com>
2016-03-01 16:35:29 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+'use strict'
+const gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    minifyCSS = require('gulp-minify-css'),
+    rename = require('gulp-rename');
+
+gulp.task('sass', () => {
+    let compileSass = sass();
+    compileSass.on('error', console.error.bind(console));
+    gulp.src('src/scss/main.scss')
+        .pipe(compileSass)
+        .pipe(rename('style.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('dist/css'));
+})
+
+gulp.task('buildJSDev', () => {
+    gulp.src('src/js/*.es6.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('shutthebox.js'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('buildJSProd', () => {
+    gulp.src('src/js/*.es6.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(uglify())
+        .pipe(concat('shutthebox.min.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', () => {
+    gulp.watch('src/sass/*.scss', ['sass']);
+    gulp.watch('src/js/*.es6.js', ['buildJSDev']);
+});
+
