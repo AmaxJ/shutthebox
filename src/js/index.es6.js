10c8e04a62392c1cf3f38348ad47f956e2d1e299
Alan <ajohnson88@gmail.com>
2016-03-02 19:39:12 -0500

    added picktile()

--- src/js/index.es6.js
+++ src/js/index.es6.js
@@ -96,8 +96,19 @@ let SHUTTHEBOX = window.SHUTTHEBOX = {};
         }
     };
 
+    STB.methods.pickTile = tile => {
+        let selectedTiles = STB.state.currentlySelectedTiles;
+        let indexOfTile = selectedTiles.indexOf(tile);
+        if (indexOfTile === -1) {
+            selectedTiles.push(tile);
+        } else {
+            selectedTiles.splice(indexOfTile, 1);
+        }
+        return getRemainingChoices();
+    }
+
     STB.methods.startGame = () => {
-        //TODO
+        STB.state.currentPlayer = STB.state.players[0];
     };
 
     STB.methods.roll = () => {
@@ -134,29 +145,29 @@ SHUTTHEBOX.methods.setPlayers(3);
 // SHUTTHEBOX.state.onlyTileOne = false;
 // console.log("2 die", SHUTTHEBOX.methods.roll());
 // console.log("2 die", SHUTTHEBOX.methods.roll());
-SHUTTHEBOX.state.players[0].score = 9;
+// SHUTTHEBOX.state.players[0].score = 9;
 // SHUTTHEBOX.state.players[1].score = 5;
-SHUTTHEBOX.state.currentPlayer = SHUTTHEBOX.state.players[2]
-SHUTTHEBOX.state.tiles["9"] = false;
-SHUTTHEBOX.state.tiles["8"] = false;
-SHUTTHEBOX.state.tiles["7"] = false;
-console.log(SHUTTHEBOX.state.players);
-console.log("Winner is: ", SHUTTHEBOX.methods.endTurn());
-// console.log(SHUTTHEBOX.helpers.getRemainingChoices(6, 3))
-// console.log(SHUTTHEBOX.helpers.getRemainingChoices(9, 5))
-// console.log(SHUTTHEBOX.helpers.getRemainingChoices(2, 0))
-// console.log(SHUTTHEBOX.helpers.getRemainingChoices(10, 7))
-/* rules:
-At the start of the game all levers or tiles are "open" (cleared, up), showing the numerals 1 to 9.
-During the game, each player plays in turn. A player begins his or her turn by throwing or rolling the die or dice into the box. If 1 is the only tile still open, the player may roll only one die. Otherwise, the player must roll both dice.
-After throwing, the player adds up the dots (pips) on the dice and then "shuts" (closes, covers) one of any combination of open numbers that equals the total number of dots showing on the dice. For example, if the total number of dots is 8, the player may choose any of the following sets of numbers (as long as all of the numbers in the set are available to be covered):
-8
-7, 1
-6, 2
-5, 3
-5, 2, 1
-4, 3, 1
-The player then rolls the dice again, aiming to shut more numbers. The player continues throwing the dice and shutting numbers until reaching a point at which, given the results produced by the dice, the player cannot shut any more numbers. At that point, the player scores the sum of the numbers that are still uncovered. For example, if the numbers 2, 3, and 5 are still open when the player throws a one, the player's score is 10 (2 + 3 + 5 = 10). Play then passes to the next player.
-After every player has taken a turn, the player with the lowest score wins.
-If a player succeeds in closing all of the numbers, he or she is said to have "Shut the Box" – the player wins immediately and the game is over.
-*/
+// SHUTTHEBOX.state.currentPlayer = SHUTTHEBOX.state.players[2]
+// SHUTTHEBOX.state.tiles["9"] = false;
+// SHUTTHEBOX.state.tiles["8"] = false;
+// SHUTTHEBOX.state.tiles["7"] = false;
+// console.log(SHUTTHEBOX.state.players);
+// console.log("Winner is: ", SHUTTHEBOX.methods.endTurn());
+console.log(SHUTTHEBOX.helpers.getRemainingChoices(6, 3))
+console.log(SHUTTHEBOX.helpers.getRemainingChoices(9, 5))
+console.log(SHUTTHEBOX.helpers.getRemainingChoices(2, 0))
+    // console.log(SHUTTHEBOX.helpers.getRemainingChoices(10, 7))
+    /* rules:
+    At the start of the game all levers or tiles are "open" (cleared, up), showing the numerals 1 to 9.
+    During the game, each player plays in turn. A player begins his or her turn by throwing or rolling the die or dice into the box. If 1 is the only tile still open, the player may roll only one die. Otherwise, the player must roll both dice.
+    After throwing, the player adds up the dots (pips) on the dice and then "shuts" (closes, covers) one of any combination of open numbers that equals the total number of dots showing on the dice. For example, if the total number of dots is 8, the player may choose any of the following sets of numbers (as long as all of the numbers in the set are available to be covered):
+    8
+    7, 1
+    6, 2
+    5, 3
+    5, 2, 1
+    4, 3, 1
+    The player then rolls the dice again, aiming to shut more numbers. The player continues throwing the dice and shutting numbers until reaching a point at which, given the results produced by the dice, the player cannot shut any more numbers. At that point, the player scores the sum of the numbers that are still uncovered. For example, if the numbers 2, 3, and 5 are still open when the player throws a one, the player's score is 10 (2 + 3 + 5 = 10). Play then passes to the next player.
+    After every player has taken a turn, the player with the lowest score wins.
+    If a player succeeds in closing all of the numbers, he or she is said to have "Shut the Box" – the player wins immediately and the game is over.
+    */
